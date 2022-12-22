c6c87cc8c100aacee63043faf42118f4b9df4bd6
wujihua <wjh50940@163.com>
2022-12-22 14:39:02 +0800

    fix: weibo api

--- api/tripartite.ts
+++ api/tripartite.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { TRIPARTITE_LINK } from 'config/app.config'
 
-const BASE_URL = process.env.NEXT_PUBLIC_DOMAIN_URL
+const ACCESS_TOKEN = process.env.NEXT_PUBLIC_WEB_ACCESS_TOKEN
 
 export const fetchWeiboList = async () => {
-  const response = await axios.get(`${BASE_URL}/weibo`, { timeout: 8000 })
+  const response = await axios.get(`https://api.weibo.com/2/statuses/user_timeline.json?access_token=${ACCESS_TOKEN}`, { timeout: 8000 })
 
   if (response.status === 200 && response.data.statuses)
     return response.data.statuses
--- next.config.js
+++ next.config.js
@@ -4,14 +4,12 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 })
 
-const ACCESS_TOKEN = process.env.NEXT_PUBLIC_WEB_ACCESS_TOKEN
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['static.evanone.site', 'tvax2.sinaimg.cn', 'cdn.evanone.me'],
+    domains: ['tvax2.sinaimg.cn', 'cdn.evanone.me'],
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -22,14 +20,6 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     })
     return config
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/weibo',
-        destination: `https://api.weibo.com/2/statuses/user_timeline.json?access_token=${ACCESS_TOKEN}`,
-      },
-    ]
   },
 }
 
